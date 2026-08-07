5708991ba4e87a0ebe724b3d74d5bc7744678af1
desarrolloclicandapp <desarrollo@clicandapp.com>
2026-08-07 10:04:29 -0300

    Remove unused RemotePAD diagnostics state

--- Teleprompt/Views/TeleprompterView.swift
+++ Teleprompt/Views/TeleprompterView.swift
@@ -25,7 +25,6 @@ struct TeleprompterView: View {
     @State var showControls = true
     @State var showCamera = false
     @State var showReaderSettings = false
-    @State var showRemoteDiagnostics = false
     @State var panelSize: CGSize = .zero
     @State var panelCenter: CGPoint = .zero
     @State var canvasSize: CGSize = .zero
